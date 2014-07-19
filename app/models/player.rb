9a4c5f4f6ebf81f3d83538f3a0c88359b8fa4075
Robert Kuśmirek <rkusmirek.2011@gmail.com>
2014-07-19 16:05:14 +0200

    small refactor

--- app/models/player.rb
+++ app/models/player.rb
@@ -13,29 +13,35 @@ class Player
 		self.dead
 	end
 
-  def admin?
-    self.role == 'admin'
-  end
-
-  def citizen?
-    self.role == 'citizen'
-  end
-
-  def werewolf?
-    self.role == 'werewolf'
-  end
-
-  def cupid?
-    self.role == 'cupid'
-  end
-
-  def girl?
-    self.role =='girl'
-  end
+  # def admin?
+  #   self.role == 'admin'
+  # end
+	#
+  # def citizen?
+  #   self.role == 'citizen'
+  # end
+	#
+  # def werewolf?
+  #   self.role == 'werewolf'
+  # end
+	#
+  # def cupid?
+  #   self.role == 'cupid'
+  # end
+	#
+  # def girl?
+  #   self.role =='girl'
+  # end
 
   def set_role!(role)
     self.update_attribute(:role, role)
   end
 
+	def self.roles
+      %w(admin citizen warewolf cupid girl)
+	end
 
+  roles.each do |role|
+    define_method("#{role}?") { self.role == role }
+  end
 end
